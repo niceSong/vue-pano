ee4844836ffdb9c572e19f4d9860040fd6844eca
1417532400@qq.com <1417532400@qq.com>
2019-08-19 00:38:25 +0800

    return old version

--- src/Pano.vue
+++ src/Pano.vue
@@ -539,7 +539,7 @@ export default {
 }
 
 .viewport > * {
-  position: relative;
+  position: absolute;
 }
 
 .hover {
@@ -561,7 +561,7 @@ export default {
 
 .controls {
   left: 10px;
-  position: relative;
+  position: absolute;
   top: 10px;
 }
 
@@ -666,7 +666,7 @@ export default {
   left: 0;
   line-height: 24px;
   margin-top: -12px;
-  position: relative;
+  position: absolute;
   right: 0;
   text-align: center;
   text-shadow: 0 0 2px #000;
